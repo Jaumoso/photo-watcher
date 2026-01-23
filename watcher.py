9a65b9d8a35db2d095d17f4e9d78940d405c0407
Jaume Civera <jaumecvr2000@gmail.com>
2026-01-23 18:28:12 +0100

    Corrected folder names

--- watcher.py
+++ watcher.py
@@ -13,9 +13,9 @@ TARGET_BASE = os.getenv("TARGET_BASE", "./target")
 IGNORE_FILES = os.getenv("IGNORE_FILES", "").split(",")
 
 MONTHS = [
-    "01. Enero", "02. Febrero", "03. Marzo", "04. Abril",
-    "05. Mayo", "06. Junio", "07. Julio", "08. Agosto",
-    "09. Septiembre", "10. Octubre", "11. Noviembre", "12. Diciembre"
+    "1. Enero", "2. Febrero", "3. Marzo", "4. Abril",
+    "5. Mayo", "6. Junio", "7. Julio", "8. Agosto",
+    "9. Septiembre", "10. Octubre", "11. Noviembre", "12. Diciembre"
 ]
 
 
